a8f869e457cec1d8762171d0eee875e71ce1ee1b
Віка Скороход <s18933@pjwstk.edu.pl>
2020-05-10 13:59:48 +0200

    painkiller

---
+++ LinqConsoleApp/LinqConsoleApp/LinqConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            d.Task1();
+            d.Task2();
+            d.Task3();
+            d.Task4();
+            d.Task5();
+            d.Task6();
+            d.Task7(); 
+            d.Task8();
+            d.Task9();
+            d.Task10();
+            d.Task11();
+            d.Task12();
+            Console.WriteLine("Mood pill: https://www.youtube.com/watch?v=2-ptLktOjrY");
+        }
+    }
+}
